443ae81ae54ce90cd8e21e9812f847807c408aec
YUEFENG WU <532490119@qq.com>
2023-12-29 09:09:14 +0800

    add linked page

---
+++ pages/08_📚_常用链接.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('优秀学习网址链接：', divider='rainbow')
+st.write("北美数学协会: [AMC8](https://maa.org/math-competitions/amc-8)")
+st.write("数学小工具: [polypad](https://mathigon.org/polypad#geometry)")
+st.write("OMEGA学习网:[omegalearn](https://www.omegalearn.org/mastering-amc8)")
+st.write("小学生科学课程：[四年级科学课本]（https://www.uen.org/emedia/resources/oer/4thGradeScience.pdf）")
+st.write("小学生科学课程：[五年级科学课本]（https://www.uen.org/emedia/resources/oer/5thGradeScience.pdf）")
+st.write("小学生科学课程：[六年级科学课本]（https://www.uen.org/emedia/resources/oer/6thGradeScience.pdf）")
--- pages/2_⚒️_几何.py
+++ pages/2_⚒️_几何.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title='My page title',
-    page_icon=':dog:'
+    page_icon=':book:'
 
 )
 
